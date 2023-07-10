6f2cb1d861b74476f382ae03247f6f8cbced2c13
iPhilBln <iPhilBln@philscloud.de>
2023-07-10 22:19:26 +0200

    testcript for stepper engine

--- Interface/test/test_engine/test_engine.cpp
+++ Interface/test/test_engine/test_engine.cpp
@@ -1,9 +1,71 @@
-#include "STEPPER_ENGINE.h"
 
-void setup(void) {
+#include "common.h"
+#include "avr8-stub.h"
+#include "app_api.h"  //only needed with flash breakpoints
 
+#define button 50
+
+void setup() {
+  //initialize GDB stub
+  //debug_init();
+
+  Serial.begin(115200);
+  delay(500);
+  pinMode(button, INPUT);
+
+  Serial.println("Start");
+
+  STEPPER_ENGINE& stepper = STEPPER_ENGINE::getInstance(ENGINE_STEP_MODE::halfstep);
+  stepper.begin();
+
+  while (true) {
+    while (digitalRead(button) == HIGH) {};
+    //Serial.println(); //Serial.println("-45:");  //Serial.flush();
+    delay(500);
+    stepper.setDegreeTarget(-45.0);
+
+    while (digitalRead(button) == HIGH) {};
+    //Serial.println(); //Serial.println("stop1:");  //Serial.flush();
+    delay(500);
+    stepper.stop();
+
+    while (digitalRead(button) == HIGH) {};
+    //Serial.println(); //Serial.println("32:");  //Serial.flush();
+    delay(500);
+    stepper.setDegreeTarget(32.0);
+
+    while (digitalRead(button) == HIGH) {};   
+    //Serial.println(); //Serial.println("Stop2:");  //Serial.flush();
+    delay(500); 
+    stepper.stop();
+
+    while (digitalRead(button) == HIGH) {};
+    //Serial.println(); //Serial.println("32:");  //Serial.flush();
+    delay(500);
+    stepper.setDegreeTarget(-32.0);
+
+    while (digitalRead(button) == HIGH) {};   
+    //Serial.println(); //Serial.println("Stop2:");  //Serial.flush();
+    delay(500); 
+    stepper.stop(); 
+
+    while (digitalRead(button) == HIGH) {};
+    //Serial.println(); //Serial.println("32:");  //Serial.flush();
+    delay(500);
+    stepper.setDegreeTarget(-15.0);
+
+      while (digitalRead(button) == HIGH) {};
+    //Serial.println(); //Serial.println("32:");  //Serial.flush();
+    delay(500);
+    stepper.setDegreeTarget(-472.0);
+
+    while (digitalRead(button) == HIGH) {};
+    //Serial.println(); //Serial.println("32:");  //Serial.flush();
+    delay(500);
+    stepper.setDegreeTarget(132.0);
+  } 
 }
 
-void loop(void) {
-    
+void loop() {
+  
 }
