560de46818c6f9a75153c1fb5092954aae9b6453
Philipp Mielke <philippmielke@icloud.com>
2023-08-03 22:47:22 +0200

    commits hinzugefügt

--- Interface/lib/simulink_interface/simulink_interface.h
+++ Interface/lib/simulink_interface/simulink_interface.h
@@ -3,8 +3,8 @@
 
 #include <Arduino.h>
 
-#define  _SERIAL0_SETUP() DDRE &= ~( _BV(PE0) ); DDRE |= _BV(PE1); PORTE |= _BV(PE1) | _BV(PE0);
-#define  _SERIAL1_SETUP() DDRD &= ~( _BV(PD2) ); DDRD |= _BV(PD3); PORTD |= _BV(PD3) | _BV(PD2);
+#define  _SERIAL0_SETUP() DDRE &= ~( _BV(PE0) ); DDRE |= _BV(PE1); PORTE |= _BV(PE1) | _BV(PE0); // enable Pull-Up -> prevent crosstalk
+#define  _SERIAL1_SETUP() DDRD &= ~( _BV(PD2) ); DDRD |= _BV(PD3); PORTD |= _BV(PD3) | _BV(PD2); // enable Pull-Up -> prevent crosstalk
 
 #define baud_rate_simulink 115200
 #define header 0b11110101
@@ -18,7 +18,7 @@ typedef union{
 void    transmitFloatToSimulink(float val);
 float   receiveFloatFromSimulink(void);
 
-void sendSimu(FLOATUNION_t val);
+void  sendSimu(FLOATUNION_t val);
 float recSimu(void);
 
 float communicationSimulink(FLOATUNION_t val);
